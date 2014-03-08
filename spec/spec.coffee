df396086876ec4fd11b9276aa6fbc388c88db51b
sol0mka <legomushroom@gmail.com>
2014-03-08 10:13:35 +0200

    add enviroment tests

--- spec/spec.coffee
+++ spec/spec.coffee
@@ -1,7 +1,15 @@
 describe 'enviroment', ->
-  it 'should have an Element', ->
+  it 'should have an Element and it\'s prototype', ->
     expect(Element).toBeDefined()
+    expect(Element.prototype.addEventListener or Element.prototype.attachEvent).toBeDefined()
 
   it 'should allow to write to Element prototype', ->
     Element.prototype.testProperty = 'test'
     expect(Element.prototype.testProperty).toBe('test')
+
+  it 'should have a dispatch event functionality', ->
+    isIE = !!document.createEventObject and !!document.fireEvent
+    isNormalBrowser = !!document.createEvent and !!document.dispatchEvent
+    expect(isIE or isNormalBrowser).toBeTrue()
+
+
--- spec/spec.js
+++ spec/spec.js
@@ -1,11 +1,18 @@
 (function() {
   describe('enviroment', function() {
-    it('should have an Element', function() {
-      return expect(Element).toBeDefined();
+    it('should have an Element and it\'s prototype', function() {
+      expect(Element).toBeDefined();
+      return expect(Element.prototype.addEventListener || Element.prototype.attachEvent).toBeDefined();
     });
-    return it('should allow to write to Element prototype', function() {
+    it('should allow to write to Element prototype', function() {
       Element.prototype.testProperty = 'test';
       return expect(Element.prototype.testProperty).toBe('test');
+    });
+    return it('should have a dispatch event functionality', function() {
+      var isIE, isNormalBrowser;
+      isIE = !!document.createEventObject && !!document.fireEvent;
+      isNormalBrowser = !!document.createEvent && !!document.dispatchEvent;
+      return expect(isIE || isNormalBrowser).toBeTrue();
     });
   });
 
