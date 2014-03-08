95f6919b2ffa5c269afcae49582f0b0b40cb9fbb
sol0mka <legomushroom@gmail.com>
2014-03-08 10:39:58 +0200

    add event listener hook

--- js/main.coffee
+++ js/main.coffee
@@ -1,1 +1,23 @@
-console.log '12'
+class Main
+	constructor:(@o={})->
+		@vars()
+		@redefineProto()
+
+	vars:->
+		@timerTags = ['input', 'textarea', 'image']
+
+	redefineProto:->
+		eventListenerFunction = Element.prototype.addEventListener or Element.prototype.attachEvent
+
+		if Element::addEventListener
+			eventListenerFunction = Element::addEventListener
+			Element::addEventListener = ->
+				console.log 'custom listener'
+				eventListenerFunction.apply(@,arguments)
+		else if Element::attachEvent
+			eventListenerFunction = Element::attachEvent
+			Element::attachEvent = ->
+				console.log 'custom listener IE'
+				eventListenerFunction.apply(@,arguments)
+
+new Main
--- js/main.js
+++ js/main.js
@@ -1,4 +1,39 @@
 (function() {
-  console.log('12');
+  var Main;
+
+  Main = (function() {
+    function Main(o) {
+      this.o = o != null ? o : {};
+      this.vars();
+      this.redefineProto();
+    }
+
+    Main.prototype.vars = function() {
+      return this.timerTags = ['input', 'textarea', 'image'];
+    };
+
+    Main.prototype.redefineProto = function() {
+      var eventListenerFunction;
+      eventListenerFunction = Element.prototype.addEventListener || Element.prototype.attachEvent;
+      if (Element.prototype.addEventListener) {
+        eventListenerFunction = Element.prototype.addEventListener;
+        return Element.prototype.addEventListener = function() {
+          console.log('custom listener');
+          return eventListenerFunction.apply(this, arguments);
+        };
+      } else if (Element.prototype.attachEvent) {
+        eventListenerFunction = Element.prototype.attachEvent;
+        return Element.prototype.attachEvent = function() {
+          console.log('custom listener IE');
+          return eventListenerFunction.apply(this, arguments);
+        };
+      }
+    };
+
+    return Main;
+
+  })();
+
+  new Main;
 
 }).call(this);
